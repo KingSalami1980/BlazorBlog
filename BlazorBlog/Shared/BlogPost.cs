a1482c029d1d20c6d302ad889a5d7fa2c4be935a
Clive Harding <clive.harding@daimler.com>
2021-09-15 18:41:18 +0200

    Added Markdig support for showing content preview using Markdown.

--- BlazorBlog/Shared/BlogPost.cs
+++ BlazorBlog/Shared/BlogPost.cs
@@ -12,7 +12,8 @@ namespace BlazorBlog.Shared
         public string Image { get; set; }
         [Required]
         public string Title { get; set; }
-        public string Content { get; set; }
+
+        public string Content { get; set; } = string.Empty;
         public string Description { get; set; }
         public string Author { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
